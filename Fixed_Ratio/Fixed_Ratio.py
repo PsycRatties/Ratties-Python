90e2ec1e4ca3cb6373f9e4bf733d7c528da721ed
Daniel Anner <daniel.anner@danstechsupport.com>
2021-01-23 11:40:55 -0500

    Fixed Ratio Updates

--- Fixed_Ratio/Fixed_Ratio.py
+++ Fixed_Ratio/Fixed_Ratio.py
@@ -77,7 +77,7 @@ def loop():
         f.write(datetime.datetime.now().strftime('%H%M%S'))
         GPIO.output(37, GPIO.HIGH)
 
-        while switchCounter2 <= fr:
+        while switchCounter2 < fr:
             print(str(GPIO.input(31)) + " | " + str(lastswitchstate2))
             if GPIO.input(31) == GPIO.HIGH and lastswitchstate2 != GPIO.input(31):
                 switchCounter2 = switchCounter2 + 1
@@ -91,7 +91,7 @@ def loop():
         f.write(datetime.datetime.now().strftime('%H%M%S'))
         GPIO.output(33, GPIO.HIGH)
 
-        while switchCounter2 <= fr:
+        while switchCounter2 < fr:
             print(str(GPIO.input(38)) + " | " + str(lastswitchstate2))
             if GPIO.input(38) == GPIO.HIGH and lastswitchstate2 != GPIO.input(38):
                 switchCounter2 = switchCounter2 + 1
